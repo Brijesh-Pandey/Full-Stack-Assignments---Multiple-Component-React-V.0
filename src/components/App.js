9555d0e7ecdf91d1bfcb3ffe16fc507d18db2c10
Brijesh Pandey <30575594+Brijesh-Pandey@users.noreply.github.com>
2020-10-30 23:36:08 +0530

    Initail Commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,17 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
-
-        return(
-            <>
-               
-            </>
-        )
-    }
+  render() {
+    return (
+      <>
+        <h1 data-ns-test="project-name">Resume Builder</h1>
+        <p data-ns-test="project-description">
+          Automated resume builder project
+        </p>
+      </>
+    );
+  }
 }
-
 
 export default App;
